cfcb103df1f01a3dad148bc5c252b981b4bcbbbd
Lucas <l.budescu@yahoo.com>
2026-07-21 16:16:19 +0300

    Made the controls

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 //Audio player
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio/miniaudio.h"
+#include "Controls.h"
+
+
 int main()
+
 {
     ma_result result;
     ma_engine engine;
-
+    ma_sound song;
+    bool isTrackPlaying;
     result = ma_engine_init(NULL, &engine);
     if (result != MA_SUCCESS) {
         return -1;
@@ -20,26 +25,67 @@ int main()
     std::cout<<"===Welcome to the best music Player(maybe not)==="<<std::endl;
     std::cout<<"   Chose a song mate!  "<<std::endl;
     int choice;
-    
+
     for(int i = 0; i < Playlist.size(); i++){
         std::cout<<i + 1<<"."<<Playlist[i]<<std::endl;
-        
     }
     std::cin>>choice;
-    switch (choice){
-        case 1:
-            ma_engine_play_sound(&engine, Playlist[0].c_str(), NULL);
-            break;
-        case 2:
-            ma_engine_play_sound(&engine, Playlist[1].c_str(), NULL);
-            break;
-        case 3:
-            ma_engine_play_sound(&engine, Playlist[2].c_str(), NULL);
-            break;
-        }
-    
-    std::cin.ignore(1);
+    if(isTrackPlaying){
+        ma_sound_uninit(&song);
+        isTrackPlaying = false;
+    }
 
+    ma_result start = ma_sound_init_from_file(
+    &engine, 
+    Playlist[choice - 1].c_str(), 
+    0, 
+    NULL, 
+    NULL, 
+    &song
+    );
+    if(start == MA_SUCCESS){
+        ma_sound_start(&song);
+        isTrackPlaying = true;
+    }
+    else{
+        std::cout<<"Failed to load"<<std::endl;
+    }
+    std::cin.ignore(1);
+    //Controls
+    int option;
+    std::cout<<"===Controls:  "<<std::endl;
+    std::cout<<"1. Next"<<std::endl;
+    std::cout<<"2. Previous"<<std::endl;
+    std::cout<<"3. Pause"<<std::endl;
+    std::cin>>option;
+    if(option == 1){
+        ma_sound_uninit(&song);
+        ma_sound_init_from_file(
+        &engine, 
+        Playlist[choice].c_str(), 
+        0, 
+        NULL, 
+        NULL, 
+        &song
+        );
+        ma_sound_start(&song);
+    }
+    else if(option == 2){
+        ma_sound_uninit(&song);
+        ma_sound_init_from_file(
+        &engine, 
+        Playlist[choice - 2].c_str(), 
+        0, 
+        NULL, 
+        NULL, 
+        &song
+        );
+        ma_sound_start(&song);
+    }
+    else if(option == 3){
+        ma_sound_uninit(&song);
+    }
+    std::cin.ignore(2, '\n');
     std::cout<<"Press Enter to quit...";
     getchar();
 
