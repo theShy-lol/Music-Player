6daedd52e82cb041b27ad92066bb818cd18301c0
Lucas <lucas.budescu01@e-uvt.ro>
2026-07-22 17:34:13 +0300

    Worked the controls logic

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<stdlib.h>
+#include"Playlist.h"
 //Audio player
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio/miniaudio.h"
-#include "Controls.h"
+
 
 
 int main()
@@ -18,18 +20,17 @@ int main()
     if (result != MA_SUCCESS) {
         return -1;
     }
-    std::vector<std::string> Playlist = {
-        "Project X.mp3", "Funk insta #1.mp3",
-        "Ichika beat.mp3"
-    };
+
     std::cout<<"===Welcome to the best music Player(maybe not)==="<<std::endl;
     std::cout<<"   Chose a song mate!  "<<std::endl;
+
     int choice;
 
     for(int i = 0; i < Playlist.size(); i++){
         std::cout<<i + 1<<"."<<Playlist[i]<<std::endl;
     }
     std::cin>>choice;
+
     if(isTrackPlaying){
         ma_sound_uninit(&song);
         isTrackPlaying = false;
@@ -51,42 +52,83 @@ int main()
         std::cout<<"Failed to load"<<std::endl;
     }
     std::cin.ignore(1);
+    
     //Controls
     int option;
     std::cout<<"===Controls:  "<<std::endl;
-    std::cout<<"1. Next"<<std::endl;
-    std::cout<<"2. Previous"<<std::endl;
-    std::cout<<"3. Pause"<<std::endl;
-    std::cin>>option;
-    if(option == 1){
-        ma_sound_uninit(&song);
-        ma_sound_init_from_file(
-        &engine, 
-        Playlist[choice].c_str(), 
-        0, 
-        NULL, 
-        NULL, 
-        &song
-        );
-        ma_sound_start(&song);
+    while(option != 5){
+        std::cout<<"1. Next"<<std::endl;
+        std::cout<<"2. Previous"<<std::endl;
+        std::cout<<"3. Pause"<<std::endl;
+        std::cout<<"4. Resume"<<std::endl;
+        std::cout<<"5. Anything else.. quit"<<std::endl;
+        std::cin>>option;
+        if(option == 1){
+            if(isTrackPlaying){
+                ma_sound_uninit(&song);
+                choice++;
+                ma_sound_init_from_file(
+                &engine, 
+                Playlist[choice - 1].c_str(), 
+                0, 
+                NULL, 
+                NULL, 
+                &song
+                );
+                ma_sound_start(&song);
+            }
+        }
+        else if(option == 2){
+            if(isTrackPlaying){
+                ma_sound_uninit(&song);
+                if(choice != 1){
+                    choice--;
+                    ma_sound_init_from_file(
+                    &engine, 
+                    Playlist[choice - 1].c_str(), 
+                    0, 
+                    NULL, 
+                    NULL, 
+                    &song
+                    );
+                    ma_sound_start(&song);
+                }
+                else{
+                    ma_sound_init_from_file(
+                    &engine, 
+                    Playlist[Playlist.size() - 1].c_str(), 
+                    0, 
+                    NULL, 
+                    NULL, 
+                    &song
+                    );
+                    ma_sound_start(&song);
+                }
+            }
+        }
+        else if(option == 3){
+            ma_sound_uninit(&song);
+            isTrackPlaying = false;
+        }
+        else if(option == 4){
+            if(!isTrackPlaying){
+            ma_sound_init_from_file(
+            &engine, 
+            Playlist[choice - 1].c_str(), 
+            0, 
+            NULL, 
+            NULL, 
+            &song
+            );
+            ma_sound_start(&song);
+            }
+            isTrackPlaying = true;
+        }
+        else{
+            ma_engine_uninit(&engine);
+        }
+        system("cls");
     }
-    else if(option == 2){
-        ma_sound_uninit(&song);
-        ma_sound_init_from_file(
-        &engine, 
-        Playlist[choice - 2].c_str(), 
-        0, 
-        NULL, 
-        NULL, 
-        &song
-        );
-        ma_sound_start(&song);
-    }
-    else if(option == 3){
-        ma_sound_uninit(&song);
-    }
-    std::cin.ignore(2, '\n');
-    std::cout<<"Press Enter to quit...";
     getchar();
 
     ma_engine_uninit(&engine);
